fa9dbeb87ecfc7642e41e597e0a40cdd1917cf24
Angelo Rocha <angelorocha0930@gmail.com>
2026-07-08 23:10:59 +0800

    feature: add mlflow

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
 
-# Start the Backend API in the background
+# Start MLflow server
+mlflow server --host 0.0.0.0 --port 5001 --backend-store-uri sqlite:///mlflow.db &
+
 python -m uvicorn app.backend_api:app --host 0.0.0.0 --port 8000 &
 
-# Start Streamlit in the foreground
+# Start Streamlit
 exec streamlit run app/streamlit_app.py \
-  --server.port=8505 \
+  --server.port=8501 \
   --server.address=0.0.0.0 \
   --server.enableCORS=false \
   --server.enableXsrfProtection=false
